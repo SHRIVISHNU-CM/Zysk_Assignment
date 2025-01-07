bb7830968098049b39200a8f996223bdb15f59c7
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-01-07 23:05:44 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import { Avatar, Box, Typography } from '@mui/material'
+import React from 'react'
+import avatar from "../images/Avatar.png"
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Navbar() {
+    return (
+        <Box sx={{
+            height: "40px",
+            maxWidth: "1000px",
+            margin: "auto",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center"
+        }}>
+            <Box sx={{
+                display: "flex",
+                gap: "30px"
+            }}>
+                <Typography variant='body1' component="h2" sx={{
+                    color: "red"
+                }}>
+                    ZYSK
+                </Typography>
+                <Typography variant='body2' component="h2">
+                    Home
+                </Typography>
+                <Typography variant='body2' component="h2">
+                    Products
+                </Typography>
+                <Typography variant='body2' component="h2">
+                    Resources
+                </Typography>
+                <Typography variant='body2' component="h2">
+                    Pricing
+                </Typography>
+
+            </Box>
+            <Box>
+                <Avatar sx={{
+                    display: { lg: "block", xs: "none" }
+                }}>
+                    <img src={avatar} width={50} />
+                </Avatar>
+                <Typography component="div" sx={{
+                    display: { lg: "none", xs: "block" }
+                }}>
+                    <MenuIcon />
+                </Typography>
+            </Box>
+        </Box>
+    )
+}
+
+export default Navbar
